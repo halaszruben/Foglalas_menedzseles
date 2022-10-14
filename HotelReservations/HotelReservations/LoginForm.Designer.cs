bcbed5ec6d510e213d6eb7fcad7ee22d5855016b
halaszruben <ruben_halasz12@yahoo.com>
2022-10-14 17:56:34 +0300

    Getting Started with the project

---
+++ HotelReservations/HotelReservations/LoginForm.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace HotelReservations
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::HotelReservations.Properties.Resources.Background;
+            this.ClientSize = new System.Drawing.Size(1252, 683);
+            this.Name = "LoginForm";
+            this.Text = "Login Form";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
